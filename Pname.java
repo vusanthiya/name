30ad50427f09960cb4171eabb2b4c1c0f0f08acf
vusanthiya <vusanthiya@gmail.com>
2019-02-22 10:40:54 +0530

    Update and rename README.md to Pname.java

---
+++ Pname.java
@@ -0,0 +1,12 @@
+public class Pname
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    int a=sc.nextInt();
+    while(a!=0)
+    {
+      System.out.println("laptop");
+    }
+  }
+}
